f9b718098af47fa0b938a045826393ca5bd3ebd2
Joshua Biddick <josh.biddick@gmail.com>
2024-10-17 10:01:39 +1300

    update repo name to pokedex-frontend

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Catchem
+# Pokedex Frontend
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.2.7.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'catchem' title`, () => {
+  it(`should have the 'pokedex-frontend' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('catchem');
+    expect(app.title).toEqual('pokedex-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, catchem');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, pokedex-frontend');
   });
 });
